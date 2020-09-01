252470843bce361dac163bbc0d5f8a8eff44bd15
DESKTOP-6KOS0A3\Mjin <johan_orcs@hotmail.com>
2020-09-01 09:13:29 +0200

    Comit mina ändringar

--- chess/Chess/ChessApp/Program.cs
+++ chess/Chess/ChessApp/Program.cs
@@ -6,6 +6,34 @@ namespace ChessApp
     {
         static void Main(string[] args)
         {
+            //name it chess app 5; 45 h into the 3rd video
+            //\n for new line
+            //min fina logiska code (swengelska)
+            for (int j = 0; j < 4; j++)
+                {
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                    //x värde
+                        Console.Write("▓▓");
+                        Console.Write("░░");
+                    }
+                    Console.WriteLine();
+                    for (int i = 0; i < 4; i++)
+                    {
+                    //y värde
+                        Console.Write("░░");
+                        Console.Write("▓▓");
+
+                    }
+
+
+                    Console.WriteLine();
+
+                }
+            Console.WriteLine();
+
+            //Sven-Eriks Matematiska Code
             for (var y = 0; y < 8; y++)
             {
                 for (var x = 0; x < 16; x++)
---
+++ session02/session02/session02Exercise01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace session02Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello me!");
+            Console.ReadKey();
+
+        }
+    }
+}
